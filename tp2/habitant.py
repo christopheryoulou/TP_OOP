3c56b2b777a9b389adc1e8a6a0563b3ad5d881f6
christopher <youlou42@gmail.com>
2026-09-22 16:54:09 +0200

    Exercice 7: Héritage : Adulte et Enfant

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -1,14 +1,18 @@
+from abc import ABC, abstractmethod
 from multipledispatch import dispatch
 
-class Habitant:
-    def __init__(self, __nom, __age, __adresse, __animaux=None):
+class Habitant(ABC):
+    def __init__(self, __nom, __prenom, __age, __adresse, __animaux=None):
         self.__nom = __nom
+        self.__prenom = __prenom
         self.__age = __age
         self.__adresse = __adresse
         self.__animaux = __animaux if __animaux is not None else {}
 
     def get_nom(self):
         return self.__nom
+    def get_prenom(self):
+        return self.__prenom
     def get_age(self):
         return self.__age
     def get_adresse (self):
@@ -18,6 +22,8 @@ class Habitant:
 
     def set_nom(self, nom):
         self.__nom = nom
+    def set_prenom(self, prenom):
+        self.__prenom = prenom
     def set_age(self, age):
         self.__age = age
     def set_adresse(self, adresse):
@@ -26,7 +32,7 @@ class Habitant:
         self.__animaux = animaux
     
     def affichage_adresse(self):
-        return f"{self.__nom} habite a {self.__adresse}"
+        return f"{self.__nom} {self.__prenom} habite a {self.__adresse}"
 
     def compte_animal(self, animal):
         return self.__animaux[animal] if animal in self.__animaux else 0
@@ -50,17 +56,54 @@ class Habitant:
         habitant.set_nom(nom)
         habitant.set_age(age)
 
+    @abstractmethod
+    def calcul_nombre_annee_avant_retraite(self):
+        pass
+        #return 62 - self.get_age() if 62 - self.get_age >= 0 else 
+        
+
+class Adulte(Habitant):
+    def __init__(self, nom, prenom, age, adresse):
+        if age < 18:
+            raise ValueError("Un adulte doit avoir au moins 18 ans")
+        super().__init__(nom, prenom, age, adresse)
+    
+    def calcul_nombre_annee_avant_retraite(self):
+        return 62 - self.get_age() if 62 - int(self.get_age()) > 0 else "deja a la retraite"
+
+class Enfant(Habitant):
+    def __init__(self, nom, prenom, age, adresse):
+        if age >= 18:
+            raise ValueError("Un enfant doit avoir moins de 18 ans")
+        super().__init__(nom, prenom, age, adresse)
+
+    def calcul_nombre_annee_avant_retraite(self):
+        return "c'est un enfant qui ne travaille probablement pas"
+
+"""
 h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
 assert h1.get_nom() == "Aldric"
 assert h1.compte_animal("vaches") == 3
 assert h1.compte_animal("moutons") == 0
-h1.affichage_adresse() # affiche "Aldric habite a Rue A"
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"    
 
 
 h1.age = 26
 assert h1.age == 26
 try:
     h1.age = -5
+    assert False, "une ValueError aurait du etre levee"
+except ValueError:
+    pass
+"""
+
+adulte = Adulte("Dupont", "Marie", 35, "Rue A")
+enfant = Enfant("Martin", "Lucas", 12, "Rue B")
+assert isinstance(adulte, Habitant)
+assert adulte.calcul_nombre_annee_avant_retraite() == 27
+assert "enfant" in enfant.calcul_nombre_annee_avant_retraite()
+try:
+    Enfant("Oups", "mince", 25, "Rue C")
     assert False, "une ValueError aurait du etre levee"
 except ValueError:
     pass
